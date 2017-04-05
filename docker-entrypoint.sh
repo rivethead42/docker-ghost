9d14a817e7d06c5d53aaa3f1812f47717044a7ab
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-04-05 14:59:18 -0500

    update end point

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
-# allow the container to be started with `--user`
-if [[ "$*" == npm*start* ]] && [ "$(id -u)" = '0' ]; then
-	chown -R user "$GHOST_CONTENT"
-	exec gosu user "$BASH_SOURCE" "$@"
-fi
-
 if [[ "$*" == npm*start* ]]; then
+
+    if [ ! -d "$GHOST_CONTENT/themes" ]; then
+        # give database a few seconds to initiate before we attempt to connect the first time
+        echo "----------> Sleeping to give database time to initialize..."
+        sleep 30
+    fi
+
 	baseDir="$GHOST_SOURCE/content"
 	for dir in "$baseDir"/*/ "$baseDir"/themes/*/; do
 		targetDir="$GHOST_CONTENT/${dir#$baseDir/}"
@@ -23,6 +24,12 @@ if [[ "$*" == npm*start* ]]; then
 			s!path.join\(__dirname, (.)/content!path.join(process.env.GHOST_CONTENT, \1!g;
 		' "$GHOST_SOURCE/config.example.js" > "$GHOST_CONTENT/config.js"
 	fi
+
+	ln -sf "$GHOST_CONTENT/config.js" "$GHOST_SOURCE/config.js"
+
+	chown -R user "$GHOST_CONTENT"
+
+	set -- gosu user "$@"
 fi
 
 exec "$@"
--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 docker build --no-cache -t rivethead42/ghost:1 -f Dockerfile .
+docker tag rivethead42/ghost:1 rivethead42/ghost:latest
+
 docker rm -f test-ghost
 docker run --name test-ghost -dit rivethead42/ghost:1 /bin/bash
